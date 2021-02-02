609f3cd4eda3b6304b5021b5ee1b333cc6a21f52
Mertcan Dursun <221903570@ogr.uludag.edu.tr>
2021-02-02 13:02:59 +0300

    Proje dosyası ekle.

---
+++ alisveris_indirim_hesaplama/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace alisveris_indirim_hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.kitapAdet = new System.Windows.Forms.TextBox();
+            this.tutarLbl = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 133);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Satın al";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kitap Adet : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tutar : ";
+            // 
+            // kitapAdet
+            // 
+            this.kitapAdet.Location = new System.Drawing.Point(197, 33);
+            this.kitapAdet.Name = "kitapAdet";
+            this.kitapAdet.Size = new System.Drawing.Size(100, 29);
+            this.kitapAdet.TabIndex = 3;
+            // 
+            // tutarLbl
+            // 
+            this.tutarLbl.AutoSize = true;
+            this.tutarLbl.Location = new System.Drawing.Point(209, 86);
+            this.tutarLbl.Name = "tutarLbl";
+            this.tutarLbl.Size = new System.Drawing.Size(57, 24);
+            this.tutarLbl.TabIndex = 4;
+            this.tutarLbl.Text = "00 TL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Info;
+            this.label3.Location = new System.Drawing.Point(12, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kitap adedi 8 TL ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(413, 270);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tutarLbl);
+            this.Controls.Add(this.kitapAdet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox kitapAdet;
+        private System.Windows.Forms.Label tutarLbl;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ alisveris_indirim_hesaplama/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace alisveris_indirim_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double tutar;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kackitap = Convert.ToInt32(kitapAdet.Text);
+
+            if (kackitap > 0 && kackitap <= 20)
+            {
+                tutar = (kackitap * 8) - (kackitap * 8 * 0.20);
+                tutarLbl.Text = tutar + " TL";
+            }
+            else if (kackitap >= 21 && kackitap <= 40)
+            {
+                tutar = (kackitap * 8) - (kackitap * 8 * 0.40);
+                tutarLbl.Text = tutar + " TL";
+            }
+            else if (kackitap >= 41)
+            {
+                tutar = (kackitap * 8) - (kackitap * 8 * 0.50);
+                tutarLbl.Text = tutar + " TL";
+            }
+        }
+    }
+}
